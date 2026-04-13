53b4321d2a72e2230dc893824e3226e3f83d01c3
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-13 12:18:08 +0530

    whole structure

---
+++ test/java/AppTest.java
@@ -0,0 +1,10 @@
+package com.example;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testIsPrime() {
+        assertTrue(App.isPrime(7));
+    }
+}
